e2fec556b8a29568166861f55caaa8211878a4c0
Mateus Kawan <mateus77kawan@gmail.com>
2023-08-01 14:48:05 -0300

    adicionando jogo da memoria no GitHub

---
+++ Jogo-da-memoria-desafio-DIO-JS/README.md
@@ -0,0 +1,9 @@
+>>>>>>>>>>>>>>># Desafio do Jogo da Memória proposto pela DIO :fire::fire::fire:
+
+
+>>>>>>>>>Neste projeto obtemos a funcionalidade do Javascript com CSS e HTML de >>>>>>>>>onde através das interações entres as class e id temos o controle de >>>>>>>>>adicionar ou remover class e id's, um com um estilo em CSS que nos leva a >>>>>>>>>ter uma interação com um jogo da memória em js.
+
+
+
+
+>>>>>>>>>>>>>>>>>>>>>É com felicidade que entrego meu desafio concluido :blush::blush::blush:
---
+++ Jogo-da-memoria-desafio-DIO-JS/assets/js/script_gamer.js
@@ -0,0 +1,71 @@
+const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+var scrore = 0;
+
+//função de virar os cards
+function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add("flip");
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+//funcção que checar quando os card são iguais.
+function checkForMatch(){
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disabledCards();
+        score += 10;
+        document.getElementById('score').innerHTML = score;
+        return;
+    }
+    unflipCards();
+}
+
+//funcao que desabilita as cartas.
+function disabledCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+//funcao que reseta o tabuleiro
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//funcao que embaralha os cards
+(function shuffle(){
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 16);
+        card.style.order = ramdomPosition;
+    })
+
+})();
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
